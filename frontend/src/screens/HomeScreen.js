80d1ea195004eae1b2cea7645aced5865be94a0d
hritik55 <hritwikbairagi55@hotmail.com>
2020-12-22 13:06:27 +0530

    Upto Seeder script

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import Product from '../components/Product'
+import axios from 'axios'
+import { Row, Col } from 'react-bootstrap'
+
+const HomeScreen = () => {
+    const [products, setProducts] = useState([]) 
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const res = await axios.get('/api/products')
+            setProducts(res.data)
+        }
+
+        fetchProducts()
+    }, [])
+    return (
+        <>
+          <h1>Latest Products</h1>
+           <Row>
+               {products.map(product => 
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
+                </Col>
+               )}
+           </Row>
+        </>
+    )
+}
+
+export default HomeScreen
